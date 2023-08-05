1168f49d611b8160f61350a0cac88889d8997775
kishanga <85829332+kishanga@users.noreply.github.com>
2023-08-05 20:41:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 st.title("Connect to Kaggle")
 
-st.subheader("Loading Netflix Movies and TV Shows dataset")
+st.subheader("Singapore Lottery Numbers")
 
 # load data
 conn = st.experimental_connection("kaggle_datasets", type=KaggleDatasetConnection)
-df = conn.get(path='shivamb/netflix-shows', filename='netflix_titles.csv', ttl=3600)
+df = conn.get(path='calven22/singapore-lottery-numbers', filename='Toto Winning Numbers.csv', ttl=3600)
 
 st.write('The dataset has {} rows and {} columns'.format(df.shape[0], df.shape[1]))
 
